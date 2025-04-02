74c50e54ad30c505dd970a8665c46ca9fb374366
Yannik Börgener <y.boergener@fixpunkt.com>
2025-04-02 23:48:47 +0200

    fix button

--- Classes/EventListener/ModifyRecordListRecordActionsEventListener.php
+++ Classes/EventListener/ModifyRecordListRecordActionsEventListener.php
@@ -7,6 +7,7 @@ use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Imaging\IconSize;
 use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 class ModifyRecordListRecordActionsEventListener {
     public function __construct(
@@ -20,11 +21,16 @@ class ModifyRecordListRecordActionsEventListener {
     public function __invoke(ModifyRecordListRecordActionsEvent $event) : void {
         if($event -> getTable() !== "fe_users") return;
         // if fe_users add button
-        $event -> setAction($this -> getButton($event -> getRecord()["uid"]), "bwfeadmin-open-frontend", "primary");
+        $event -> setAction(
+            $this -> getButton($event -> getRecord()["uid"]),
+            "bwfeadmin-open-frontend",
+            "primary",
+            "edit"
+        );
     }
 
     protected function getButton(int $userUid) : string {
-        return '<a class="btn btn-default t3js-record-confirm" target="_blank" href="' . $this->buildFrontendUri($userUid) . '"'
+        return '<a class="btn btn-default" target="_blank" href="' . $this->buildFrontendUri($userUid) . '"'
         . ' title="Login in FE">'
         . $this -> iconFactory -> getIcon(
             'feadmin-switch-to-user',
